5a7b0e3f0a1afe3527d0052a7c7a48550cfee1fa
Onesmus Wambua <onesmuswambua747@gmail.com>
2026-04-02 21:18:16 +0300

    added payments in books and downloads

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(helmet())
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://christianitycrossroads.onrender.com"],
     credentials: true
 }))
 app.use('/api/user',userRouter)
