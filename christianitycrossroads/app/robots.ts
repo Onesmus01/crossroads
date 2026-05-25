db48b82797b5c44565f7059c468b415b61ad7e31
Onesmus Wambua <onesmuswambua747@gmail.com>
2026-05-25 10:56:01 +0300

    perfomance optimization

--- christianitycrossroads/app/robots.ts
+++ christianitycrossroads/app/robots.ts
@@ -1,7 +1,7 @@
 import { type MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'http://localhost:3000'; // Change this to your actual domain in production
+    const baseUrl = 'https://www.christianitycrossroads.com'; // Change this to your actual domain in production
 
     return {
         rules: [
